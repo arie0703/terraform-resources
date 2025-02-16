cf90a96d60034ec3dda1790d8d1f4f78d243fbe1
K.A. <kaiseiarimura0703@gmail.com>
2025-02-16 11:38:25 +0900

    SQSをトリガーとして設定

--- services/sqs-lambda/main.tf
+++ services/sqs-lambda/main.tf
@@ -1,19 +1,18 @@
-# ローカルにあるlambdaのソースコード
 data "archive_file" "default" {
   type        = "zip"
   source_dir  = "${path.module}/src"
   output_path = "${path.module}/src/index.zip"
 }
 
-# AWSへ作るlambda function
 resource "aws_lambda_function" "default" {
-  function_name    = "${var.service_name}-function"
-  filename         = data.archive_file.default.output_path
-  source_code_hash = data.archive_file.default.output_base64sha256
-  runtime          = "nodejs22.x"
-  role             = aws_iam_role.lambda.arn
-  handler          = "index.handler"
-  timeout          = 60
+  function_name                  = "${var.service_name}-function"
+  filename                       = data.archive_file.default.output_path
+  source_code_hash               = data.archive_file.default.output_base64sha256
+  runtime                        = "nodejs22.x"
+  role                           = aws_iam_role.lambda.arn
+  handler                        = "index.handler"
+  reserved_concurrent_executions = 0
+  timeout                        = 60
 
   environment {
     variables = {
@@ -28,6 +27,11 @@ resource "aws_lambda_permission" "default" {
   function_name = aws_lambda_function.default.function_name
   principal     = "sqs.amazonaws.com"
   source_arn    = aws_sqs_queue.default.arn
+}
+
+resource "aws_lambda_event_source_mapping" "default" {
+  event_source_arn = aws_sqs_queue.default.arn
+  function_name    = aws_lambda_function.default.arn
 }
 
 resource "aws_cloudwatch_log_group" "lambda" {
