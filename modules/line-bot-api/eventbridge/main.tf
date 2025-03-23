1434c539f042b34ca3797ec18871dbdfe437c14a
K.A. <kaiseiarimura0703@gmail.com>
2025-03-24 07:29:06 +0900

    API GWをコメントアウト

--- modules/line-bot-api/eventbridge/main.tf
+++ modules/line-bot-api/eventbridge/main.tf
@@ -1,11 +1,10 @@
 resource "aws_scheduler_schedule" "default" {
   name                         = var.schedule_name
   schedule_expression_timezone = "Asia/Tokyo"
-  schedule_expression          = "cron(0 1 * * ? *)"
+  schedule_expression          = "cron(0 10 25 * ? *)"
 
   flexible_time_window {
-    mode                      = "FLEXIBLE"
-    maximum_window_in_minutes = 1
+    mode = "OFF"
   }
 
   target {
--- services/line-bot-api/main.tf
+++ services/line-bot-api/main.tf
@@ -5,16 +5,18 @@ module "lambda_cost_confirmation" {
   scheduler_arn = module.eventbridge_cost_confirmation.scheduler_arn
 }
 
-module "apigw" {
-  source = "../../modules/line-bot-api/apigw"
-
-  service_name                 = var.service_name
-  cost_confirmation_invoke_arn = module.lambda_cost_confirmation.invoke_arn
-}
-
 module "eventbridge_cost_confirmation" {
   source = "../../modules/line-bot-api/eventbridge"
 
   schedule_name = "line-cost-confirmation-schedule"
   lambda_arn    = module.lambda_cost_confirmation.function_arn
 }
+
+# API Gatewayは現状Line botで使用していないので一旦コメントアウト
+
+# module "apigw" {
+#   source = "../../modules/line-bot-api/apigw"
+
+#   service_name                 = var.service_name
+#   cost_confirmation_invoke_arn = module.lambda_cost_confirmation.invoke_arn
+# }
